687434b0398eb45398bda5fa36424cf093e6475a
Berkay Yerdelen <berkayyerdelen@gmail.com>
2019-10-29 19:31:54 +0300

    jwt handlers

---
+++ src/Application/Core/Comman/Security/Jwt/JwtHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using Core.Comman.Extensions;
+using Core.Comman.Security.Encryption;
+using Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Core.Comman.Security.Jwt
+{
+    public class JwtHelper : ITokenHelper
+    {
+        public IConfiguration Configuration { get; set; }
+        private TokenOptions _tokenOptions;
+        private DateTime _accessTokenExpiration;
+
+        public JwtHelper(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+            _accessTokenExpiration = DateTime.Now.AddMinutes(_tokenOptions.AccesTokenExpiration);
+        }
+        public AccessToken CreateToken(Customer customer, List<OperationClaim> operationClaims)
+        {
+            var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
+            var signingCredentials = SigningCredentialsHelper.CreateSigningCredentials(securityKey);
+            var jwt = CreateJwtSecurityToken(_tokenOptions, customer, signingCredentials, operationClaims);
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var token = jwtSecurityTokenHandler.WriteToken(jwt);
+            return new AccessToken()
+            {
+                Token = token,
+                Expiration = _accessTokenExpiration
+            };
+        }
+
+        public JwtSecurityToken CreateJwtSecurityToken(TokenOptions tokenOptions, Customer customer,
+            SigningCredentials signingCredentials, List<OperationClaim> operationClaims)
+        {
+            var jwt = new JwtSecurityToken(
+                issuer: tokenOptions.Issuer,
+                audience: tokenOptions.Audience,
+                expires: _accessTokenExpiration,
+                notBefore: DateTime.Now,
+                claims: SetClaims(customer, operationClaims),
+                signingCredentials: signingCredentials
+            );
+            return jwt;
+        }
+
+        private IEnumerable<Claim> SetClaims(Customer customer, List<OperationClaim> operationClaims)
+        {
+            var claims = new List<Claim>();
+            claims.AddNameIdentifier(customer.Id.ToString());
+            claims.AddEmail(customer.Email);
+            claims.AddName(customer.FullName);
+            claims.AddRoles(operationClaims.Select(c => c.Name).ToArray());
+            return claims;
+        }
+    }
+}
