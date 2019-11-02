ad0aa336756a4f733656ab25164b3c90be837f61
Berkay Yerdelen <berkayyerdelen@gmail.com>
2019-11-02 08:04:00 +0300

    categorycontroller

--- src/WabApi/Controllers/CategoryController.cs
+++ src/WabApi/Controllers/CategoryController.cs
@@ -22,33 +22,28 @@ namespace WabApi.Controllers
         }
         [HttpPost]
         public async Task<Unit> CreateCategory(CreateCategoryCommand request, CancellationToken ct)
-        {
-            return await _mediator.Send(request, ct);
-        }
+        => await _mediator.Send(request, ct);
+       
 
         [HttpDelete]
         public async Task<Unit> DeleteCategory(DeleteCategoryCommand request, CancellationToken ct)
-        {
-            return await _mediator.Send(request, ct);
-        }
+            => await _mediator.Send(request, ct);
+       
 
         [HttpPut]
         public async Task<Unit> UpdateCategory(UpdateCategoryCommand request, CancellationToken ct)
-        {
-            return await _mediator.Send(request, ct);
-        }
+        => await _mediator.Send(request, ct);
+       
 
         [HttpGet]
         public async Task<CategoryListViewModel> GetCategoryList(CancellationToken ct)
-        {
-            return await _mediator.Send(new GetCategoryListQuery(), ct);
-        }
+        => await _mediator.Send(new GetCategoryListQuery(), ct);
+      
 
         [HttpGet]
         [Route("findbyname/{name}")]
         public async Task<FindCategoryByNameViewModel> GetCategoryInfo(string name, CancellationToken ct)
-        {
-            return await _mediator.Send(new FindCategoryByNameQuery(name), ct);
-        }
+        =>  await _mediator.Send(new FindCategoryByNameQuery(name), ct);
+       
     }
 }
