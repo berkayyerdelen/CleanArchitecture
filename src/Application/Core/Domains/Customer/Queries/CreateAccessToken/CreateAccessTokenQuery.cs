7259e005e3a8e8f2021cf2a726896cfca4193153
Berkay Yerdelen <berkayyerdelen@gmail.com>
2019-11-10 00:41:48 +0300

    Revert "TokenHandler"
    This reverts commit a70e00a1dfb4d368d4ea46d89b34928784d1f56b.

--- src/Application/Core/Domains/Customer/Queries/CreateAccessToken/CreateAccessTokenQuery.cs
+++ src/Application/Core/Domains/Customer/Queries/CreateAccessToken/CreateAccessTokenQuery.cs
@@ -1,43 +1,10 @@
-﻿using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Core.Comman.Security.Jwt;
-using Core.Interface;
+﻿
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 
 namespace Core.Domains.Customer.Queries.CreateAccessToken
 {
-    public class CreateAccessTokenQuery : IRequest<CreateAccesTokenViewModel>
+    public class CreateAccessTokenQuery
     {
-        public int CustomerId { get; set; }
 
-        public CreateAccessTokenQuery(int customerId)
-            => CustomerId = customerId;
-
-        public class Handler : IRequestHandler<CreateAccessTokenQuery, CreateAccesTokenViewModel>
-        {
-            private readonly IApplicationDbContext _context;
-            private readonly ITokenHelper _tokenHelper;
-
-            public Handler(IApplicationDbContext context, ITokenHelper tokenHelper)
-                => (_context,_tokenHelper) = (context,tokenHelper);
-
-            public async Task<CreateAccesTokenViewModel> Handle(CreateAccessTokenQuery request, CancellationToken cancellationToken)
-            {
-                var claims = (from operationClaim in _context.Set<Entities.OperationClaim>()
-                    join customerOperationClaim in _context.Set<Entities.CustomerOperationClaim>() on operationClaim.Id
-                        equals customerOperationClaim.OperationClaimId
-                    where customerOperationClaim.CustomerId == request.CustomerId
-                    select new Entities.OperationClaim { Id = operationClaim.Id, Name = operationClaim.Name }).ToList();
-                var customer = await _context.Set<Entities.Customer>().SingleOrDefaultAsync(x => x.Id == request.CustomerId,cancellationToken);
-                var accestoken = _tokenHelper.CreateToken(customer, claims);
-                return new CreateAccesTokenViewModel()
-                {
-                    Token = accestoken.Token,
-                    ExpirationDate = accestoken.Expiration
-                };
-            }
-        }
     }
 }
