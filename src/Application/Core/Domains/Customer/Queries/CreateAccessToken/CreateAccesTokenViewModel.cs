a70e00a1dfb4d368d4ea46d89b34928784d1f56b
Berkay Yerdelen <berkayyerdelen@gmail.com>
2019-11-09 14:14:34 +0300

    TokenHandler

---
+++ src/Application/Core/Domains/Customer/Queries/CreateAccessToken/CreateAccesTokenViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MediatR;
+
+namespace Core.Domains.Customer.Queries.CreateAccessToken
+{
+    public class CreateAccesTokenViewModel
+    {
+        public string Token { get; set; }
+        public DateTime ExpirationDate { get; set; }
+    }
+}
