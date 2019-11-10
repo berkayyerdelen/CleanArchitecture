44e34727a0248a2c9156fad0a246e2b1aae75161
Berkay Yerdelen <berkayyerdelen@gmail.com>
2019-11-10 17:36:21 +0300

    create customer  action

--- src/WabApi/Controllers/CustomerController.cs
+++ src/WabApi/Controllers/CustomerController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Core.Domains.Customer.Commands.CreateCustomer;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WabApi.Controllers
 {
@@ -6,5 +10,16 @@ namespace WabApi.Controllers
     [ApiController]
     public class CustomerController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public CustomerController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("CreateCustomer")]
+        public async Task<Unit> CreateCustomer(CreateCustomerCommand request, CancellationToken ct)
+            => await _mediator.Send(request, ct);
+
     }
 }
