0bf72f17aef7589de3e8cc73c3111b1fd182cc01
Berkay Yerdelen <berkayyerdelen@gmail.com>
2019-11-18 19:20:14 +0300

    seperate class

--- src/Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Core.Interface;
 using Entities;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
-using Newtonsoft.Json;
 
 namespace Persistence
 {
@@ -126,32 +123,5 @@ namespace Persistence
             return SaveChangesAsync();
         }
 
-    }
-    public class AuditEntry
-    {
-        public AuditEntry(EntityEntry entry)
-        {
-            Entry = entry;
-        }
-
-        public EntityEntry Entry { get; }
-        public string TableName { get; set; }
-        public Dictionary<string, object> KeyValues { get; } = new Dictionary<string, object>();
-        public Dictionary<string, object> OldValues { get; } = new Dictionary<string, object>();
-        public Dictionary<string, object> NewValues { get; } = new Dictionary<string, object>();
-        public List<PropertyEntry> TemporaryProperties { get; } = new List<PropertyEntry>();
-
-        public bool HasTemporaryProperties => TemporaryProperties.Any();
-
-        public Audit ToAudit()
-        {
-            var audit = new Audit();
-            audit.TableName = TableName;
-            audit.DateTime = DateTime.UtcNow;
-            audit.KeyValues = JsonConvert.SerializeObject(KeyValues);
-            audit.OldValues = OldValues.Count == 0 ? null : JsonConvert.SerializeObject(OldValues);
-            audit.NewValues = NewValues.Count == 0 ? null : JsonConvert.SerializeObject(NewValues);
-            return audit;
-        }
     }
 }
---
+++ src/Infrastructure/Persistence/AuditEntry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Entities;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Newtonsoft.Json;
+
+namespace Persistence
+{
+    public class AuditEntry
+    {
+        public AuditEntry(EntityEntry entry)
+        {
+            Entry = entry;
+        }
+
+        public EntityEntry Entry { get; }
+        public string TableName { get; set; }
+        public Dictionary<string, object> KeyValues { get; } = new Dictionary<string, object>();
+        public Dictionary<string, object> OldValues { get; } = new Dictionary<string, object>();
+        public Dictionary<string, object> NewValues { get; } = new Dictionary<string, object>();
+        public List<PropertyEntry> TemporaryProperties { get; } = new List<PropertyEntry>();
+
+        public bool HasTemporaryProperties => TemporaryProperties.Any();
+
+        public Audit ToAudit()
+        {
+            var audit = new Audit();
+            audit.TableName = TableName;
+            audit.DateTime = DateTime.UtcNow;
+            audit.KeyValues = JsonConvert.SerializeObject(KeyValues);
+            audit.OldValues = OldValues.Count == 0 ? null : JsonConvert.SerializeObject(OldValues);
+            audit.NewValues = NewValues.Count == 0 ? null : JsonConvert.SerializeObject(NewValues);
+            return audit;
+        }
+    }
+}
