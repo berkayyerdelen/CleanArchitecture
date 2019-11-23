a3a286e1e3dbcf2b4e0d8fd88165e2ddba908f41
Berkay Yerdelen <berkayyerdelen@gmail.com>
2019-11-24 00:17:35 +0300

    Create Product validator fix

--- src/Application/Core/Domains/Product/Commands/CreateProduct/CreateProductCommandValidator.cs
+++ src/Application/Core/Domains/Product/Commands/CreateProduct/CreateProductCommandValidator.cs
@@ -8,7 +8,7 @@ namespace Core.Domains.Product.Commands.CreateProduct
         {
             RuleFor(x => x.CategoryId).NotEmpty().NotNull();
             RuleFor(x => x.Discontinued).NotNull();
-            RuleFor(x => x.ProductName).Length(100).NotNull();
+            RuleFor(x => x.ProductName).MaximumLength(100).NotNull();
         }
     }
 }
