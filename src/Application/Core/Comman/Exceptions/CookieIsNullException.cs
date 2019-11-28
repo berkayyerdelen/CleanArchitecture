1c9a533a4452306adb35949aa5ac3fcf5ba7c216
Berkay Yerdelen <berkayyerdelen@gmail.com>
2019-11-28 20:43:38 +0300

    jwt cookie check controller

---
+++ src/Application/Core/Comman/Exceptions/CookieIsNullException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Comman.Exceptions
+{
+    public class CookieIsNullException: Exception
+    {
+        public CookieIsNullException()
+            : base($"Cookie is null or empty")
+        {
+        }
+    }
+}
--- src/Application/Core/Comman/Infrastructure/AppUserIdSession.cs
+++ src/Application/Core/Comman/Infrastructure/AppUserIdSession.cs
@@ -2,6 +2,7 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
+using Core.Comman.Exceptions;
 using Core.Comman.Interface.AppUserSession;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
@@ -16,13 +17,17 @@ namespace Core.Comman.Infrastructure
         public AppUserIdSession(IHttpContextAccessor httpContextAccessor)
             => (_httpContextAccessor) = (httpContextAccessor);
 
-
+           
         public int JwtUserIdParse()
         {
             try
             {
                 JwtSecurityTokenHandler _jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
                 var jwt = _httpContextAccessor.HttpContext.Request.Cookies["jwt"];
+                if (String.IsNullOrEmpty(jwt))
+                {
+                    throw new CookieIsNullException();
+                }
                 var readableToken = _jwtSecurityTokenHandler.CanReadToken(jwt);
                 var parSecurityToken = _jwtSecurityTokenHandler.ReadToken(jwt) as JwtSecurityToken;
                 return Convert.ToInt32(parSecurityToken?.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value);
