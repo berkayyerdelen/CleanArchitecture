b69636c46322115cb202975cc27e5436299c3782
Berkay Yerdelen <berkayyerdelen@gmail.com>
2019-11-29 13:02:17 +0300

    cacher imp

---
+++ src/Application/Core/Comman/Caching/Cacher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Core.Comman.Interface.Caching;
+using Couchbase.Extensions.Caching;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Core.Comman.Caching
+{
+    public class Cacher : ICaching
+    {
+        public IDistributedCache _cacher { get; set; }
+        public Cacher(IDistributedCache cacher) => _cacher = cacher;
+
+        public string GetCachedData(string cacheKey)
+        {
+            var cachedData = _cacher.Get<object>(cacheKey);
+            return JsonConvert.SerializeObject(cachedData);
+
+        }
+
+        public void SetCache(string cacheKey, object obj)
+        {
+            _cacher.Set(cacheKey, obj, new DistributedCacheEntryOptions() { AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1) });
+        }
+    }
+}
