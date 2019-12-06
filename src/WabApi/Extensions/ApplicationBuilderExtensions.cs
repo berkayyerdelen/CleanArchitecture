ac6785a8d2f08177941398a0b01197edb3f64642
Berkay Yerdelen <berkayyerdelen@gmail.com>
2019-12-06 21:12:54 +0300

    hangfire arrow function

--- src/WabApi/Extensions/ApplicationBuilderExtensions.cs
+++ src/WabApi/Extensions/ApplicationBuilderExtensions.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Core.Comman.Infrastructure.HangFire;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
@@ -21,12 +18,8 @@ namespace WabApi.Extensions
     public class SecurityHeadersMiddleware
     {
         private readonly RequestDelegate next;
-
-        public SecurityHeadersMiddleware(RequestDelegate next)
-        {
-            this.next = next;
-        }
-
+        public SecurityHeadersMiddleware(RequestDelegate next) 
+            => this.next = next;
         public async Task Invoke(HttpContext context)
         {
             context.Response.Headers.Add(
@@ -41,26 +34,18 @@ namespace WabApi.Extensions
         }
 
     }
-
     public class HanfireMiddleware
     {
         private readonly RequestDelegate next;
         private readonly IMediator _mediator;
-        public HanfireMiddleware(RequestDelegate next, IMediator mediator)
-        {
-            _mediator = mediator;
-            this.next = next;
-        }
-
-
+        public HanfireMiddleware(RequestDelegate next, IMediator mediator) 
+            => (_mediator, this.next) = (mediator, next);
+        
         public async Task Invoke(HttpContext context)
         {
             //var fireandForgetJob = new FireAndForgetJob();
-
             //var delayedJob = new DelayedJob();
-
             var recurringJob = new RecurringJob(_mediator);
-
             //var continuationsJob = new ContinuationsJob();
             await next(context);
         }
