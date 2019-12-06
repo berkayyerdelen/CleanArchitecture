465bbca30c905bf7acaaa1faee693a8753ed1d18
Berkay Yerdelen <berkayyerdelen@gmail.com>
2019-12-06 21:07:33 +0300

    refactoring audit methods on save changes

--- src/Infrastructure/Persistence/AuditEntry.cs
+++ src/Infrastructure/Persistence/AuditEntry.cs
@@ -13,7 +13,6 @@ namespace Persistence
         {
             Entry = entry;
         }
-
         public EntityEntry Entry { get; }
         public string TableName { get; set; }
         public Dictionary<string, object> KeyValues { get; } = new Dictionary<string, object>();
@@ -25,12 +24,14 @@ namespace Persistence
 
         public Audit ToAudit()
         {
-            var audit = new Audit();
-            audit.TableName = TableName;
-            audit.DateTime = DateTime.UtcNow;
-            audit.KeyValues = JsonConvert.SerializeObject(KeyValues);
-            audit.OldValues = OldValues.Count == 0 ? null : JsonConvert.SerializeObject(OldValues);
-            audit.NewValues = NewValues.Count == 0 ? null : JsonConvert.SerializeObject(NewValues);
+            var audit = new Audit()
+            {
+                TableName = TableName,
+                DateTime = DateTime.UtcNow,
+                KeyValues = JsonConvert.SerializeObject(KeyValues),
+                OldValues = OldValues.Count == 0 ? null : JsonConvert.SerializeObject(OldValues),
+                NewValues= NewValues.Count == 0 ? null : JsonConvert.SerializeObject(NewValues)
+            };
             return audit;
         }
     }
