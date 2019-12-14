2b95a12429046f1fea5f72edaf2cb706132c85e0
Berkay Yerdelen <berkayyerdelen@gmail.com>
2019-12-14 19:24:57 +0300

    imp update customer

---
+++ src/Application/Core/Domains/Customer/Commands/UpdateCustomer/UpdateCustomerCommand.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+
+namespace Core.Domains.Customer.Commands.UpdateCustomer
+{
+    public class UpdateCustomerCommand : IRequest<Unit>
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public bool IsActive { get; set; }
+        public UpdateCustomerDetailsDto CustomerDetailsDto { get; set; }
+    }
+
+    public class UpdateCustomerDetailsDto
+    {
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string ContactTitle { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public UpdateCustomerCommand CustomerDto { get; set; }
+        public string Fax { get; set; }
+    }
+}
---
+++ src/Application/Core/Domains/Customer/Commands/UpdateCustomer/UpdateCustomerHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Security.Principal;
+using System.Threading;
+using System.Threading.Tasks;
+using Core.Comman.Extensions;
+using Core.Comman.Interface;
+using Core.Comman.Security;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Domains.Customer.Commands.UpdateCustomer
+{
+    public class UpdateCustomerHandler : IRequest<UpdateCustomerCommand>
+    {
+        public class Handler : IRequestHandler<UpdateCustomerCommand, Unit>
+        {
+            public IApplicationDbContext _context { get; set; }
+            public Handler(IApplicationDbContext context)
+                => _context = context;
+            public async Task<Unit> Handle(UpdateCustomerCommand request, CancellationToken cancellationToken)
+            {
+                byte[] passwordHash, passwordSalt;
+                HashingHelper.CreatePasswordHash(request.Password, out passwordHash, out passwordSalt);
+                var customer = _context.Set<Entities.Customer>().SingleOrDefaultAsync
+                    (x => x.PasswordHash == passwordHash && x.Email == request.Email, cancellationToken).Result;
+
+                customer.CustomerDetails.Address = request.CustomerDetailsDto.Address;
+                customer.CustomerDetails.City = request.CustomerDetailsDto.City;
+                customer.CustomerDetails.CompanyName = request.CustomerDetailsDto.CompanyName;
+                customer.CustomerDetails.ContactName = request.CustomerDetailsDto.ContactName;
+                customer.CustomerDetails.ContactTitle = request.CustomerDetailsDto.ContactTitle;
+                customer.CustomerDetails.Country = request.CustomerDetailsDto.Country;
+                customer.CustomerDetails.Fax = request.CustomerDetailsDto.Fax;
+                customer.CustomerDetails.Phone = request.CustomerDetailsDto.Phone;
+                customer.CustomerDetails.PostalCode = request.CustomerDetailsDto.PostalCode;
+                customer.CustomerDetails.Region = request.CustomerDetailsDto.Region;
+                customer.FullName = request.FullName;
+                customer.IsActive = request.IsActive;
+                await _context.SaveChangesAsync(true, cancellationToken);
+                return Unit.Value;
+            }
+        }
+    }
+}
