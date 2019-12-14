bf34200bbbfd6880314224ed7d0ef1bf30ab6f39
Berkay Yerdelen <berkayyerdelen@gmail.com>
2019-12-14 18:36:38 +0300

    imp Iqueryable extension

---
+++ src/Application/Core/Comman/Extensions/QueryableExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Core.Comman.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> PageBy<T>(this IQueryable<T> query, int skipCount, int maxResultCount)
+        {
+            if (query == null)
+                throw new ArgumentNullException("query");
+
+            return query.Skip(skipCount).Take(maxResultCount);
+        }
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool condition, Expression<Func<T, bool>> predicate)
+        {
+            return condition
+                ? query.Where(predicate)
+                : query;
+        }
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool condition, Expression<Func<T, int, bool>> predicate)
+        {
+            return condition
+                ? query.Where(predicate)
+                : query;
+        }
+    }
+}
