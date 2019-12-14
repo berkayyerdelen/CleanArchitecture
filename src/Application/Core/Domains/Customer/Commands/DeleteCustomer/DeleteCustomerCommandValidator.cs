4f0c5c233d27575ae920bbdc1a5107e16e0b2e67
Berkay Yerdelen <berkayyerdelen@gmail.com>
2019-12-14 18:40:57 +0300

    delete customer validator imp

---
+++ src/Application/Core/Domains/Customer/Commands/DeleteCustomer/DeleteCustomerCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Core.Domains.Customer.Commands.DeleteCustomer
+{
+    public class DeleteCustomerCommandValidator: AbstractValidator<DeleteCustomerCommandHandler>
+    {
+        public DeleteCustomerCommandValidator()
+        {
+            RuleFor(x => x.Email).NotEmpty().NotNull();
+            RuleFor(x => x.Password).NotNull().NotEmpty();
+        }
+    }
+}
