bc4d583a4ba67563775d39dfce235712ab224f01
Berkay Yerdelen <berkayyerdelen@gmail.com>
2019-12-14 23:25:21 +0300

    imp of updatecustomer command /mapping dto to entity

--- src/Application/Core/Domains/Customer/Commands/UpdateCustomer/UpdateCustomerHandler.cs
+++ src/Application/Core/Domains/Customer/Commands/UpdateCustomer/UpdateCustomerHandler.cs
@@ -2,6 +2,7 @@
 using System.Security.Principal;
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using Core.Comman.Extensions;
 using Core.Comman.Interface;
 using Core.Comman.Security;
@@ -15,27 +16,29 @@ namespace Core.Domains.Customer.Commands.UpdateCustomer
         public class Handler : IRequestHandler<UpdateCustomerCommand, Unit>
         {
             public IApplicationDbContext _context { get; set; }
-            public Handler(IApplicationDbContext context)
-                => _context = context;
+            private readonly IMapper _mapper;
+            public Handler(IApplicationDbContext context , IMapper mapper)
+                => (_context, _mapper) = (context, mapper);
+
             public async Task<Unit> Handle(UpdateCustomerCommand request, CancellationToken cancellationToken)
             {
                 byte[] passwordHash, passwordSalt;
                 HashingHelper.CreatePasswordHash(request.Password, out passwordHash, out passwordSalt);
-                var customer = _context.Set<Entities.Customer>().SingleOrDefaultAsync
+                var query = _context.Set<Entities.Customer>().SingleOrDefaultAsync
                     (x => x.PasswordHash == passwordHash && x.Email == request.Email, cancellationToken).Result;
-
-                customer.CustomerDetails.Address = request.CustomerDetailsDto.Address;
-                customer.CustomerDetails.City = request.CustomerDetailsDto.City;
-                customer.CustomerDetails.CompanyName = request.CustomerDetailsDto.CompanyName;
-                customer.CustomerDetails.ContactName = request.CustomerDetailsDto.ContactName;
-                customer.CustomerDetails.ContactTitle = request.CustomerDetailsDto.ContactTitle;
-                customer.CustomerDetails.Country = request.CustomerDetailsDto.Country;
-                customer.CustomerDetails.Fax = request.CustomerDetailsDto.Fax;
-                customer.CustomerDetails.Phone = request.CustomerDetailsDto.Phone;
-                customer.CustomerDetails.PostalCode = request.CustomerDetailsDto.PostalCode;
-                customer.CustomerDetails.Region = request.CustomerDetailsDto.Region;
-                customer.FullName = request.FullName;
-                customer.IsActive = request.IsActive;
+                var customer = _mapper.Map<UpdateCustomerCommand, Entities.Customer>(request, query);
+                //customer.CustomerDetails.Address = request.CustomerDetailsDto.Address;
+                //customer.CustomerDetails.City = request.CustomerDetailsDto.City;
+                //customer.CustomerDetails.CompanyName = request.CustomerDetailsDto.CompanyName;
+                //customer.CustomerDetails.ContactName = request.CustomerDetailsDto.ContactName;
+                //customer.CustomerDetails.ContactTitle = request.CustomerDetailsDto.ContactTitle;
+                //customer.CustomerDetails.Country = request.CustomerDetailsDto.Country;
+                //customer.CustomerDetails.Fax = request.CustomerDetailsDto.Fax;
+                //customer.CustomerDetails.Phone = request.CustomerDetailsDto.Phone;
+                //customer.CustomerDetails.PostalCode = request.CustomerDetailsDto.PostalCode;
+                //customer.CustomerDetails.Region = request.CustomerDetailsDto.Region;
+                //customer.FullName = request.FullName;
+                //customer.IsActive = request.IsActive;
                 await _context.SaveChangesAsync(true, cancellationToken);
                 return Unit.Value;
             }
